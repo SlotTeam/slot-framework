09a707cba3572533ad52e69dd1a9ea8934203431
Carlos Echeverría <CarlosEchev07@gmail.com>
2015-03-04 00:41:32 -0700

    Continue woking on #2 issue, Grunt integration. Adding code to create the `.usageMap` file, this file will be used to store the relation beteewn `pages` and `fragments`.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 var path = require("path"),
-    slot = require('./slot.json');
+    fs = require('fs'),
+    slot = require('./slot.json'),
+    usageMap = require('./.usageMap');
 
 module.exports = function (grunt) {
 
@@ -7,6 +9,7 @@ module.exports = function (grunt) {
 
     // Project configuration.
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         watch: {
             configFiles: {
                 files: [ 'Gruntfile.js' ],
@@ -16,6 +19,12 @@ module.exports = function (grunt) {
             },
             html: {
                 files: ['www/**/*.html']
+            },
+            fragmentRootDir: {
+                files: ['design/fragment/**/*.html']
+            },
+            metaData: {
+                files: ['design/bind/**/*.json']
             }
         }
     });
@@ -23,16 +32,55 @@ module.exports = function (grunt) {
         grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
 
         if (target != 'configFiles') {
+
             var url = path.join('/', filepath);
-            url = url.replace(slot.framework.webRootDir, '');
+
+            if (target == 'html') {
+                url = url.replace(slot.framework.webRootDir, '');
+
+                // Extract all attributes fragment types, using the regex ({@)[^\s]*(@})
+                fs.readFile( filepath ,'binary' /*'utf8'*/, function(err, data) {
+
+                    var re = /({@)[^\s]*(@})/g; // Regex to find fragments used on this page
+                    var str = data; //'hi {@welcomeMsg@}    </div>    <br>    {@comboBox@} goodby\nhi {@welcome Msg@}    </div>    <br>    {@comb-oBox@} goodby\nhi {@welcome!Msg@}    </div>    <br>    {@combo_Box@} goodby\nhi {@welcomeM~sg@}    </div>    <br>    {@com$boBox@} goodby\nhi {@welcomeMsg@}    </div>    <br>    {@combo\nBox@} goodby';
+                    var m, fragments = [], index;
+
+                    while ((m = re.exec(str)) != null) {
+                        console.log(m[0]);
+
+                        if (m.index === re.lastIndex) {
+                            re.lastIndex++;
+                        }
+
+                        fragments[index++] = m[0];
+                    }
+
+                    // TODO: update the .usageMap file
+                    // fs.writeFile(slotJsonFile, JSON.stringify(slotJson, null, 4), function (err) {
+                    //     //if (err) {
+                    //     //    pretty.failed("Fail creating '%s'", fragment);
+                    //     //    throw err;
+                    //     //}
+                    //     //pretty.done("Fragment '%s' created on '%s'", fragment, 'todo_path');
+                    //     callback(err)
+                    // });
+                });
+            }
+            else if (target == 'fragmentRootDir') {
+                url = url.replace(slot.framework.fragmentRootDir, '');
+            }
+            else {
+                url = url.replace(slot.framework.metaData, '');
+            };
 
             grunt.log.writeln('url: ' + url);
 
-            buildPage(url, function() {
-                grunt.log.writeln('url: done - ' + url);
-            })
+            if (target == 'html')
+                buildPage(url, function(content) {
+                    // grunt.log.writeln('url: done - ' + url);
+                    grunt.log.writeln('received content: ' + content.length + 'Kb');
+                })
         };
-
     });
 
     grunt.registerTask('default', 'Initiate services for Slot Framework', function() {
@@ -45,62 +93,48 @@ module.exports = function (grunt) {
             )
         );
 
+        grunt.config('watch.fragmentRootDir.files',
+            path.join( '.'
+                , slot.framework.fragmentRootDir
+                , '/**/**/**/**/**/**/**/**/**/'
+                , '*.html'
+            )
+        );
+
+        grunt.config('watch.metaData.files',
+            path.join( '.'
+                , slot.framework.metaData
+                , '/**/**/**/**/**/**/**/**/**/'
+                , '*.json'
+            )
+        );
+
         grunt.task.run('watch');
     });
-};
 
+    function buildPage(url, callbackEnd) {
+        var http = require('http');
+        var options = {
+            host: '127.0.0.1',
+            path: url,
+            port :2000
+        };
 
-function buildPage(url, callbackEnd) {
-    var http = require('http');
+        callback = function(response) {
+            var str = '';
 
-    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-    var options = {
-        host: '127.0.0.1',
-        path: url,
-        port :2000
-    };
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
 
-    callback = function(response) {
-        var str = '';
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function () {
+                // console.log(str);
+                callbackEnd(str);
+            });
+        }
 
-        //another chunk of data has been recieved, so append it to `str`
-        response.on('data', function (chunk) {
-            str += chunk;
-        });
-
-        //the whole response has been recieved, so we just print it out here
-        response.on('end', function () {
-            console.log(str);
-
-            callbackEnd(str);
-        });
+        http.request(options, callback).end();
     }
-
-    http.request(options, callback).end();
-}
-
-//function buildPage(callback) {
-//    var http = require('http');
-//
-//    //The url we want is: 'www.random.org/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-//    var options = {
-//        host: 'www.random.org',
-//        path: '/integers/?num=1&min=1&max=10&col=1&base=10&format=plain&rnd=new'
-//    };
-//
-//    callback = function(response) {
-//        var str = '';
-//
-//        //another chunk of data has been recieved, so append it to `str`
-//        response.on('data', function (chunk) {
-//            str += chunk;
-//        });
-//
-//        //the whole response has been recieved, so we just print it out here
-//        response.on('end', function () {
-//            console.log(str);
-//        });
-//    }
-//
-//    http.request(options, callback).end();
-//}
+};
