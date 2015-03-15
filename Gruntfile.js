31e90d5cef284c49ce0988bab0785729143374d9
Carlos Echeverría <CarlosEchev07@gmail.com>
2015-03-14 22:46:10 -0700

    Woking on #2 issue, Grunt integration. Errors on multiple simultaneous Build Request have been corrected.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 var path = require("path"),
-    fs = require('fs'),
-    sortObj = require('sort-object'),
-    async = require('async'),
     slot = require('./slot.json'),
-    usageMap = require('./.usageMap.json');
+    usageMap = require('./.usageMap.json'),
+    slotApi = require('slot-framework'),
+    gruntTasks = slotApi.GruntTasks.create()
+    ;
+
 
 module.exports = function (grunt) {
 
@@ -34,101 +35,22 @@ module.exports = function (grunt) {
             }
         }
     });
-    grunt.event.on('watch', function(action, filepath, target) {
-        grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+    grunt.event.on('watch', function(action, filePath, target) {
+        console.log(target + ': ' + filePath + ' has ' + action);
 
         if (target != 'configFiles') {
-
-            var url = path.join('/', filepath);
-
-            grunt.log.writeln('url: ' + url);
-
-
             if (target == 'html') {
-                url = url.replace(slot.framework.webRootDir, '');
-
-                // Extract all fragment type attributes, using the regex ({@)[^\s]*(@})
-                fs.readFile(filepath, 'binary', function(err, data) {
-
-                    var re = /({@)[^\s]*(@})/g; // Regex to find fragments used on this page
-                    var str = data;
-                    var m, fragments = [], fragmentName, index = 0;
-
-                    while ((m = re.exec(str)) != null) {
-                        // console.log(m[0]);
-
-                        if (m.index === re.lastIndex) {
-                            re.lastIndex++;
-                        }
-
-                        fragmentName = m[0].replace("{@","").replace("@}","");
-                        console.log(fragmentName);
-
-                        // Add only declared fragments
-                        if (slot.fragments[fragmentName]) {
-                            fragments[index++] = fragmentName;
-
-                            // Create fragment reference if does not exist
-                            !usageMap.fragment[fragmentName] && (usageMap.fragment[fragmentName] = []);
-
-                            // Add relation beteewn fragment and page if it does not exists
-                            usageMap.fragment[fragmentName].indexOf(url) < 0 && (usageMap.fragment[fragmentName].push(url));
-                        };
-                    }
-
-                    // Update used fragments on .usageMap.json file
-                    usageMap.page[url] = fragments.sort();
-                    usageMap.page = sortObj(usageMap.page);
-
-                    // Update the .usageMap file
-                    fs.writeFile('./.usageMap.json', JSON.stringify(usageMap, null, 4), function (err) {
-
-                        grunt.log.writeln('usageMap updated: ' + (err ? ' failed ' : 'success') + ' for ' + url);
-
-                        if (!err)
-                            buildPage(url, function(content) {
-                                // grunt.log.writeln('url: done - ' + url);
-                                grunt.log.writeln('received content: ' + content.length + 'Kb');
-                            });
-                    });
-                });
+                gruntTasks.buildPageFromHtml(slot, usageMap, filePath);
             }
             else if (target == 'fragmentRootDir') {
-                url = url.replace(slot.framework.fragmentRootDir, '');
-
-                var page,
-                    fragmentName = url.split("/").pop().split(".")[0];
-
-                grunt.log.writeln('fragment: ' + fragmentName);
-
-                // Add only declared fragments
-                if (slot.fragments[fragmentName] && usageMap.fragment[fragmentName]) {
-
-                    async.eachSeries(usageMap.fragment[fragmentName], function( page, callback) {
-
-                        grunt.log.writeln('fragment: ' + fragmentName + ' - page:' + page);
-
-                        setTimeout(function() {
-                            buildPage(page, function(content) {
-                                grunt.log.writeln('received content: ' + content.length + 'Kb - ' + (new Date()).getTime() );
-
-                                callback();
-                            });
-                        }, 1000);
-
-                    }, function(err){
-                        // if any of the file processing produced an error, err would equal that error
-                        if( err ) {
-                            // One of the iterations produced an error, the processing will now stop.
-                            console.log('Building pages has failed');
-                        } else {
-                            console.log('All pages have been built successfully');
-                        }
-                    });
-                }
+                gruntTasks.buildFragmentFromHtml(slot, usageMap, filePath);
             }
             else {
-                url = url.replace(slot.framework.metaData, '');
+                //url = url.replace(slot.framework.metaData, '');
+                /**
+                 * TODO:
+                 *  1.  Build pages from fragments metaDataBinding
+                 */
             }
         }
     });
@@ -159,32 +81,14 @@ module.exports = function (grunt) {
             )
         );
 
+        /**
+         * TODO:
+         *  1.  Create PID file to control start/Stop for the grunt tasks
+         *  1.1 If an instance is running, we need to stop that previous
+         *      instance and then start the new instance.
+         *      Else case, we continue to start the new instance.
+         */
+
         grunt.task.run('watch');
     });
-
-    function buildPage(url, callbackEnd) {
-        var http = require('http');
-        var options = {
-            host: '127.0.0.1',
-            path: url,
-            port :2001
-        };
-
-        callback = function(response) {
-            var str = '';
-
-            //another chunk of data has been recieved, so append it to `str`
-            response.on('data', function (chunk) {
-                str += chunk;
-            });
-
-            //the whole response has been recieved, so we just print it out here
-            response.on('end', function () {
-                // console.log(str);
-                callbackEnd(str);
-            });
-        }
-
-        http.request(options, callback).end();
-    }
 };
